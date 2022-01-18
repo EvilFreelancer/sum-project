6bf1cb242ca747f289b4025681d09f56b72ba708
Paul Rock <paul@drteam.rocks>
2022-01-18 20:19:32 +0300

    Db port fix

--- backend/src/Db.py
+++ backend/src/Db.py
@@ -11,7 +11,7 @@ class Db:
 
     def __init__(self):
         self.host = os.getenv('DB_HOST')
-        self.port = os.getenv('DB_PORT')
+        self.port = int(os.getenv('DB_PORT'))
         self.database = os.getenv('DB_NAME')
         self.user = os.getenv('DB_USER')
         self.password = os.getenv('DB_PASS')
